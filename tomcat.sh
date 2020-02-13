47635f93ad2aed53a70377f56fce0476637a2621
rafaelbonilha <jrbonilha@hotmail.com>
2020-02-13 10:44:19 -0300

    exec tomcat server with terraform 5

---
+++ tomcat.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+
+if [ ! -f ${CATALINA_HOME}/scripts/.tomcat_admin_created ]; then
+	${CATALINA_HOME}/scripts/create_admin_user.sh
+	${CATALINA_HOME}/bin/catalina.sh run
+	else	
+	${CATALINA_HOME}/bin/catalina.sh run
+fi
+		
+
