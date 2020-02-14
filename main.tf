fb9361ffe5e882b5e69bc7f96def704c0e851901
rafaelbonilha <jrbonilha@hotmail.com>
2020-02-14 14:26:01 -0300

    exec apache tomcat server with terraform test 8

--- main.tf
+++ main.tf
@@ -12,5 +12,5 @@ resource "docker_container" "tomcat7" {
 
 # Find the latest precise image.
 resource "docker_image" "tomcat7" {
-  name = "rafaelbonilha85/tomcat7:latest"
+  name = "rafaelbonilha85/tomcat7:7.0.99"
 }
